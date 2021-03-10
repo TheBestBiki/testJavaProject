aa7efea3aa29464c676cef9a7cac7bfc8822db88
Biki <924872898@qq.com>
2021-03-10 23:55:20 +0800

    byte测试

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
     2.右键项目，选择Build module '项目名'
     3.File -> project structure... ,给project选项配置好jdk，以及output目录；然后选择modules,选择sources,然后点击src，
     再点击蓝色的sources，这时右边会多出蓝色的src相关的字，就可以了。 保存，ok就可以运行了。
+    
+# 自20210310起写的记录
+    测试了字符串转byte数组时数据的存储情况，包括原码，反码，补码等
+    com.byteTest.ByteTest.testByteSave
--- src/com/byteTest/ByteTest.java
+++ src/com/byteTest/ByteTest.java
@@ -1,6 +1,10 @@
 package com.byteTest;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 /**
+ * 创建byte的三种方式：https://blog.csdn.net/jdbfvhxx/article/details/95377739
  * @author biki
  * @date 2021/3/8
  */
@@ -18,14 +22,89 @@ public class ByteTest {
 
      */
 
-    public static void main(String[] args) {
-        String chinese = "中";
-        for (byte aByte : chinese.getBytes()) {
-            System.out.println(aByte);
-        }
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        testByteSave();
 
     }
 
+    /**
+     * 测试字符串转byte数组时数据存储的情况
+     * 结论：1.
+     * @throws UnsupportedEncodingException
+     */
+    private static void testByteSave() throws UnsupportedEncodingException {
+        // 获得字符串的对应UTF-8编码表的16进制数
+        String str = URLEncoder.encode("中", "UTF-8").replace("%", "");
+        System.out.println(str); // 得字符串"中"对应的16进制为E4B8AD
+        System.out.println("================");
+        String chinese = "中";
+        // 因为UTF-8里一个字符串占3个字节，所以转为byte数组时，实际上数组的长度为3，即 E4 B8 AD
+        // 但是byte数组不可能存成{E4,B8,AD},因为byte的存储范围只有-128~127
+        // 所以实际上，字符串转byte数组时，byte数组里存的是补码，
+        // 即：E4原码：228(16进制)=1110 0100，反码为：1001 1011 ，补码：1001 1100 = -28
+        // 同理得B8的补码为：-72，AD的补码为-83
+        // 所以byte数组里，存的是{-28,-72,-83}
+        byte[] bytes = chinese.getBytes();
+        // 循环3次打印的结果：-28 -72 -83
+        for (byte aByte : bytes) {
+            System.out.println(aByte);
+        }
+        // byte数组转字符串时，因为字符串占3个字节，因此会将每3个byte，即3个字节合为一个字符串
+        // 该过程中，byte数组的每个byte会先取补码，再将补码合起来
+        // 即：-28=1001 1100，得补码为：1110 0100 = 228 = E4，发现与"中"的16进制里前面2位一样
+        // 同理得-72的补码为B8，-83的补码为AD，3个合起来就是E4B8AD，再通过该编码，到UTF-8编码表里，找到了字符"中"
+        String aa = new String(bytes);
+        System.out.println("String的方法获得数组，再将数组转String后的结果->"+aa);
+
+        System.out.println("================负数转byte");
+        byte[] testBytes ={-28,-72,-83};
+        System.out.println(new String(testBytes)); // 中
+
+        System.out.println("================快速求byte的补码");
+        System.out.println(testBytes[0] & 0xff); // 228,这种方式可以直接得-28的补码，原理见https://www.cnblogs.com/zhangcaiwang/p/14251795.html
+        System.out.println("================测试正数的byte");
+        byte[] byte2 ={97};
+        System.out.println(byte2[0] & 0xff); //结果还是本身，正数的补码也还是其本身
+
+        System.out.println("================16进制转byte");
+        byte[] byte16 ={(byte)0xE4,(byte)0xB8,(byte)0xAD};
+        for (byte b : byte16) {
+            System.out.println(b);
+        }
+
+
+    }
+
+    /**
+     * 字符串转16进制
+     * 不加replaceAll的话，结果是类似这样的形式：%E4%B8%AD
+     * @param str 字符串
+     * @param encode 编码格式
+     * @return
+     */
+    public static String strTo16(String str,String encode) {
+        try {
+            return URLEncoder.encode("中", "utf-8").replace("%", "");
+        }catch (Exception e){
+            System.out.println(e);
+            return "字符串转16进制失败";
+        }
+    }
+
+    /**
+     * 字符串转化成为16进制字符串
+     * @param s
+     * @return
+     */
+    public static String strTo16(String s) {
+        String str = "";
+        for (int i = 0; i < s.length(); i++) {
+            int ch = (int) s.charAt(i);
+            String s4 = Integer.toHexString(ch);
+            str = str + s4;
+        }
+        return str;
+    }
 
 
 }
